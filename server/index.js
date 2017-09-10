660460fc4d7d426d6dddb69f238983b55c86486c
ValeriyZubairov <ValeriyIU5-12@yandex.ru>
2017-09-10 15:51:38 +0300

    From Heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 'use strict';
 const express = require("express");
-
 const app = express();
+var fs = require('fs');
 
-app.use(express.static("../public"));
+
+app.use(express.static("public"));
+
+//app.get('game.html', (req, res) => {
+//	res.send('game_page');
+//});
+
+//app.get('*', (req, res) => {
+//	res.send('404');
+//});
+
 
 
 app.listen(process.env.PORT || '8080', () => {
