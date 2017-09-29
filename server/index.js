22f9f7edcc53b9185da96c025c69c75957c1802b
Nika <nika_persiyanova@mail.ru>
2017-09-29 13:10:03 +0300

    some fixes

--- server/index.js
+++ server/index.js
@@ -5,25 +5,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-const users = {
-    'vasya@mail.ru': {
-        email: 'vasya@mail.ru',
-        password: 'password',
-        score: 10,
-    },
-    'petya@mail.ru': {
-        email: 'petya@mail.ru',
-        password: 'password',
-        score: 1000,
-    },
-    'nika@mail.ru': {
-        email: 'nika@mail.ru',
-        password: 'password',
-        score: 500000,
-    }
-};
-
-
 app.get('/users', function (req, res) {
     const scorelist = Object.values(users)
         .sort((l, r) => r.score - l.score)
