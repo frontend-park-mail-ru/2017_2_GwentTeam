c2da1d92403dd27a3b02bb8308785d088ed6ec7d
Nika <nika_persiyanova@mail.ru>
2017-09-29 13:27:38 +0300

    server/index.js

--- server/index.js
+++ server/index.js
@@ -5,19 +5,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/users', function (req, res) {
-    const scorelist = Object.values(users)
-        .sort((l, r) => r.score - l.score)
-        .map(user => {
-            return {
-                email: user.email,
-                score: user.score,
-            };
-        });
-
-    res.json(scorelist);
-});
-
 const port = process.env.PORT || 8000;
 
 app.listen(port, function () {
