8edce0dd561b0a7c16b0791c224c8b860c4a0296
Nika <nika_persiyanova@mail.ru>
2017-11-10 18:47:59 +0300

    singleplayer fixes

---
+++ public/views/game-view/game-strategy/multiplayer.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import Strategy from './strategy.js';
+import GameScene from './game-scene.js';
+import bus from '../../../modules/event-bus.js';
+/**
+* @module GameView
+* @extends BaseView
+*/
+export default class MultiPlayerStrategy extends Strategy {
+    
+}
---
+++ public/views/game-view/game-strategy/strategy.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+* GameStrategy
+* @name GameStrategy
+* @class GameStrategy
+*/
+export default class GameStrategy {
+    constructor() {
+        console.log('GameStrategy.fn');
+    }
+}
