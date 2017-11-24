b90918f162f75c85893d62416c4bd30e052c41ff
Paulik8 <ikhotin@mail.ru>
2017-11-24 17:44:39 +0300

    mobile styles

--- public/main.js
+++ public/main.js
@@ -14,6 +14,11 @@ import Router from './modules/router.js';
 
 import './blocks/form/index.styl';
 import './styles.styl';
+import './views/application-view/application.styl';
+
+const Background = document.createElement('div');
+Background.setAttribute('class', 'background__img');
+document.body.appendChild(Background);
 
 const userService = new UserService();
 const application = new ApplicationView(document.body);
