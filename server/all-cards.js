ceadfb17585483228b70e77215e2b484bfd3d2d9
Nika <persiyanova.v@yandex.ru>
2017-12-15 16:31:48 +0300

    something

--- server/all-cards.js
+++ server/all-cards.js
@@ -1,161 +1,155 @@
 module.exports = {
-    'ghoul': {
+    ghoul: {
         score: 1,
         index: 'ghoul1',
         faction: 'monster',
         type: 'b'
     },
-    'nekker': {
+    nekker: {
         score: 2,
         index: 'nekker1',
         faction: 'monster',
         type: 'b'
     },
-    'wyvern': {
+    wyvern: {
         score: 2,
         index: 'wyvern',
         faction: 'monster',
         type: 'd'
     },
-    'foglet': {
+    foglet: {
         score: 2,
         index: 'foglet',
         faction: 'monster',
         type: 'b'
     },
-    'gargoyle': {
+    gargoyle: {
         score: 2,
         index: 'gargoyle',
         faction: 'monster',
         type: 'd'
     },
-    'cockatrice': {
+    cockatrice: {
         score: 2,
         index: 'cockatrice',
         faction: 'monster',
         type: 'd'
     },
-    // 'harpy': {
-    //     score: 2,
-    //     index: 'harpy',
-    //     faction: 'monster',
-    //     type: 1
-    // },
-    'endrega': {
+    endrega: {
         score: 2,
         index: 'endrega',
         faction: 'monster',
         type: 'd'
     },
-    'vampire_bruxa': {
+    vampire_bruxa: {
         score: 4,
         index: 'vampire_bruxa',
         faction: 'monster',
         type: 'b'
     },
-    'vampire_fleder': {
+    vampire_fleder: {
         score: 4,
         index: 'vampire_fleder',
         faction: 'monster',
         type: 'b'
     },
-    'vampire_garkain': {
+    vampire_garkain: {
         score: 4,
         index: 'vampire_garkain',
         faction: 'monster',
         type: 'b'
     },
-    'vampire_ekimmara': {
+    vampire_ekimmara: {
         score: 4,
         index: 'vampire_ekimmara',
         faction: 'monster',
         type: 'b'
     },
-    'arachas': {
+    arachas: {
         score: 4,
         index: 'arachas1',
         faction: 'monster',
         type: 'b'
     },
-    'botchling': {
+    botchling: {
         score: 4,
         index: 'botchling',
         faction: 'monster',
         type: 'b'
     },
-    'forktail': {
+    forktail: {
         score: 5,
         index: 'forktail',
         faction: 'monster',
         type: 'b'
     },
-    'griffin': {
+    griffin: {
         score: 5,
         index: 'griffin',
         faction: 'monster',
         type: 'b'
     },
-    'werewolf': {
+    werewolf: {
         score: 5,
         index: 'werewolf',
         faction: 'monster',
         type: 'b'
     },
-    'frightener': {
+    frightener: {
         score: 5,
         index: 'frightener',
         faction: 'monster',
         type: 'b'
     },
-    'ice_giant': {
+    ice_giant: {
         score: 5,
         index: 'ice_giant',
         faction: 'monster',
         type: 'c'
     },
-    'grave_hag': {
+    grave_hag: {
         score: 5,
         index: 'grave_hag',
         faction: 'monster',
         type: 'd'
     },
-    'crone_whispess': {
+    crone_whispess: {
         score: 6,
         index: 'crone_whispess',
         faction: 'monster',
         type: 'b'
     },
-    'crone_brewess': {
+    crone_brewess: {
         score: 6,
         index: 'crone_brewess',
         faction: 'monster',
         type: 'b'
     },
-    'crone_weavess': {
+    crone_weavess: {
         score: 6,
         index: 'crone_weavess',
         faction: 'monster',
         type: 'b'
     },
-    'arachas_behemoth': {
+    arachas_behemoth: {
         score: 6,
         index: 'arachas_behemoth',
         faction: 'monster',
         type: 'c'
     },
-    'fire_elemental': {
+    fire_elemental: {
         score: 6,
         index: 'fire_elemental',
         faction: 'monster',
         type: 'c'
     },
-    'fiend': {
+    fiend: {
         score: 6,
         index: 'fiend',
         faction: 'monster',
         type: 'b'
     },
-    'earth_elemental': {
+    earth_elemental: {
         score: 6,
         index: 'earth_elemental',
         faction: 'monster',
